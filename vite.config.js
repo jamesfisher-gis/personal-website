a7ddd1b632523d5fe83ed2af35934f10a0c18508
James <jafisher32@gmail.com>
2024-06-24 23:06:38 -0400

    conditional config

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        format: 'amd',
-        amd: {
-          autoId: true,
-          basePath: '/personal-website/'
-        }
+export default defineConfig(({ command, mode }) => {
+  if (command === 'build') {
+      return {
+        base: '/personal-website/',
+        plugins: [
+          vue(),
+          nightwatchPlugin()
+        ]
       }
+  } else {
+    return {
+      plugins: [
+        vue(),
+        nightwatchPlugin()
+      ]
     }
-  },
-  plugins: [
-    vue(),
-    nightwatchPlugin()
-  ]
+  }
 })
-
-
